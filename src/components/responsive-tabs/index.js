eb657e6938fc3cd1f03d61876c0f1d452455a12a
Paul Walton <paul@denmandigital.com>
2020-11-16 13:09:58 -0800

    Refactored Responsive tabs, added hasNotification callback prop

--- src/components/responsive-tabs/index.js
+++ src/components/responsive-tabs/index.js
@@ -1,54 +1,112 @@
-import { GUTESTRAP_TEXT_DOMAIN } from "../../const";
-
 import classNames from "classnames";
-import { sprintf } from "sprintf-js";
 
-import { __, _n } from "@wordpress/i18n";
+import { __ } from "@wordpress/i18n";
+import { TabPanel, Dashicon } from "@wordpress/components";
 
-import { Fragment, useMemo } from "@wordpress/element";
-import { registerBlockType, createBlock } from "@wordpress/blocks";
-import { InspectorControls, InnerBlocks } from "@wordpress/block-editor";
-import { PanelBody, Panel, PanelRow, SelectControl, TabPanel } from "@wordpress/components";
-import { createHigherOrderComponent } from "@wordpress/compose";
-
-import { Icon } from "../fontawesome-icons";
+import { GUTESTRAP_TEXT_DOMAIN } from "../../const";
 
 import "./editor.scss";
 
-export const ResponsiveTabs = ({ className, children }) => {
+const breakpoints = ["xs", "sm", "md", "lg", "xl", "xxl"];
+
+export const ResponsiveTabs = ({ className, children, hasNotification = () => false }) => {
+	let haveNotifications = breakpoints.filter(hasNotification);
+	haveNotifications =
+		Array.isArray(haveNotifications) && haveNotifications.filter((value) => breakpoints.includes(value));
+	if (haveNotifications.length === 0) {
+		haveNotifications = false;
+	}
 	return (
 		<TabPanel
 			className={classNames("gutestrap-responsive-tabs", className)}
 			tabs={[
 				{
+					name: "xs",
+					title: <span>{__("Base", GUTESTRAP_TEXT_DOMAIN)}</span>,
+					// title: <Dashicon icon="smartphone" />,
+					// className: "has-icon-title",
+					label: __("Base", GUTESTRAP_TEXT_DOMAIN),
+					description: __("Settings for devices of all sizes.", GUTESTRAP_TEXT_DOMAIN),
+					breakpoint: "xs",
+					className: classNames({
+						"has-notification": haveNotifications && haveNotifications.includes("xs"),
+					}),
+				},
+				{
 					name: "sm",
-					title: __("Base", GUTESTRAP_TEXT_DOMAIN),
-					className: "",
+					title: (
+						<span>
+							<Dashicon icon="smartphone" style={{ transform: "scale(-1, 1) rotate(90deg)" }} />
+						</span>
+					),
+					className: classNames("has-icon-title", {
+						"has-notification": haveNotifications && haveNotifications.includes("sm"),
+					}),
+					label: __("Mobile landscape", GUTESTRAP_TEXT_DOMAIN),
+					description: __(
+						"Settings for landscape mobile devices and larger. 576px wide and up.",
+						GUTESTRAP_TEXT_DOMAIN
+					),
 					breakpoint: "sm",
 				},
 				{
-					name: "xs",
-					title: <Icon style="regular" icon="mobile" alt={__("Extra-small", GUTESTRAP_TEXT_DOMAIN)} size="lg" />,
-					className: "has-icon-title",
-					breakpoint: "xs",
-				},
-				{
 					name: "md",
-					title: <Icon style="regular" icon="tablet" alt={__("Medium", GUTESTRAP_TEXT_DOMAIN)} size="lg" />,
-					className: "has-icon-title",
+					title: (
+						<span>
+							<Dashicon icon="tablet" />
+						</span>
+					),
+					className: classNames("has-icon-title", {
+						"has-notification": haveNotifications && haveNotifications.includes("md"),
+					}),
+					label: __("Tablet", GUTESTRAP_TEXT_DOMAIN),
+					description: __("Settings for tablets and larger devices. 768px wide and up.", GUTESTRAP_TEXT_DOMAIN),
 					breakpoint: "md",
 				},
 				{
 					name: "lg",
-					title: <Icon style="regular" icon="laptop" alt={__("Large", GUTESTRAP_TEXT_DOMAIN)} size="lg" />,
-					className: "has-icon-title",
+					title: (
+						<span>
+							<Dashicon icon="tablet" style={{ transform: "scale(-1, 1) rotate(90deg)" }} />
+						</span>
+					),
+					className: classNames("has-icon-title", {
+						"has-notification": haveNotifications && haveNotifications.includes("lg"),
+					}),
+					label: __("Tablet landscape", GUTESTRAP_TEXT_DOMAIN),
+					description: __(
+						"Settings for landscape tablets and larger devices. 992px wide and up.",
+						GUTESTRAP_TEXT_DOMAIN
+					),
 					breakpoint: "lg",
 				},
 				{
 					name: "xl",
-					title: <Icon style="regular" icon="desktop" alt={__("Extra-large", GUTESTRAP_TEXT_DOMAIN)} size="lg" />,
-					className: "has-icon-title",
+					title: (
+						<span>
+							<Dashicon icon="laptop" />
+						</span>
+					),
+					className: classNames("has-icon-title", {
+						"has-notification": haveNotifications && haveNotifications.includes("xl"),
+					}),
+					label: __("Laptop", GUTESTRAP_TEXT_DOMAIN),
+					description: __("Settings for laptops and larger devices. 1200px wide and up.", GUTESTRAP_TEXT_DOMAIN),
 					breakpoint: "xl",
+				},
+				{
+					name: "xxl",
+					title: (
+						<span>
+							<Dashicon icon="desktop" />
+						</span>
+					),
+					className: classNames("has-icon-title", {
+						"has-notification": haveNotifications && haveNotifications.includes("xxl"),
+					}),
+					label: __("Desktop", GUTESTRAP_TEXT_DOMAIN),
+					description: __("Settings for desktop devices and larger. 1200px wide and up.", GUTESTRAP_TEXT_DOMAIN),
+					breakpoint: "xxl",
 				},
 			]}
 		>
