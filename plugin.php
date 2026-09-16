ee5c6f35cd6dc979082f25d84733de9f222f44ee
Paul Walton <paul@denmandigital.com>
2026-09-16 16:40:46 -0700

    v2.2.21

--- plugin.php
+++ plugin.php
@@ -7,7 +7,7 @@
  * Description: Supercharge your Gutenberg layouts with Bootstrap Grid (and other goodies).
  * Author: Denman Digital
  * Author URI: https://denman.digital
- * Version: 2.2.20
+ * Version: 2.2.21
  * Requires at least: 6.0
  * Tested up to: 6.8
  * Requires PHP: 8.1
